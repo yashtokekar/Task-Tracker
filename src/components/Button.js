667e8f1074498d36cb06499d4167fcaba6295550
Yash Tokekar <yash.tokekar@gmail.com>
2021-08-12 02:00:00 +0530

    adding files

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+
+const Button = ({color,text,onClick}) => {
+
+    return <button  onClick={onClick} className="btn" style={{backgroundColor: color}}>{text}</button>
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    color: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+
+}
+
+export default Button
